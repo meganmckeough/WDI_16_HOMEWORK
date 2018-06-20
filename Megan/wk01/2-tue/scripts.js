dfdb40a260c1c92f0e0f90760ad5bd89f6b46009
Megan McKeough <megan.mckeough@gmail.com>
2018-06-20 22:37:12 +1000

    Megan 01 Tues hw Vars and Loops

---
+++ Megan/wk01/2-tue/scripts.js
@@ -0,0 +1,68 @@
+//The Fortune Teller
+
+// var jobTitle = "Web Developer extraordinaire";
+// var myLocation = "Melbourne";
+// var partnerName = "Benedict Cumberbatch";
+// var numberOfKids = "no";
+
+// console.log("You will be a " + jobTitle + " in " + myLocation);
+
+
+// The Age Calculator
+
+// var currentYear = 2018
+// var birthYear = 2000
+
+// var currentAge1 = (currentYear - birthYear);
+// var currentAge2 = (currentAge1 - 1);
+
+// console.log("They are either " + currentAge1 + " or " + currentAge2);
+
+//The Lifetime Supply Calculator
+
+// var myCurrentAge = 32;
+// var myMaximumAge = 90;
+// var amountPerDay = 3;
+// var amountForLife = (myMaximumAge - myCurrentAge) * 365 * amountPerDay;
+
+// console.log("You will need " + amountForLife + " to last you until the ripe old age of " + myMaximumAge);
+
+// The Even/Odd Reporter 
+
+// for (number = 0; number <= 20; number++) {
+// 	if (number % 2 === 0) {
+// 		console.log(number + " is even");
+// 	} else {
+// 		console.log(number + " is odd");
+// 	}
+// }
+
+// Multiplication tables 
+
+// for (number = 0; number <= 10; number ++) {
+// 	var result = (number * 9);
+// 	console.log(number + " * 9 " + (" = ") + result);
+// }
+
+// Loop for all - need help with this !! cannot get it working
+
+// for (number = 0; number <= 10; number++) {
+// 	for (multiplyNumber = 1; multiplyNumber <= 10; number++) {
+// 		var result = (multiplyNumber * number);
+// 	}
+// 	console.log(number + " * " + multiplyNumber + " = " + result);
+// }
+
+// Arrays
+
+// var movies = ["The Shining", "Call Me By Your Name", "Clue", "The Social Network"];
+// var rank = ["1st", "2nd", "3rd", "4th"];
+
+// for (i = 0; i < 4; i++) {
+// 	console.log(movies[i] + " is " + "my " + rank[i] + " choice");
+// }
+
+
+
+
+
