db155ddd4df443a676cde934e7979b17bb28d7b8
Megan McKeough <megan.mckeough@gmail.com>
2018-06-21 10:48:37 +1000

    Megan 02 Wed hw Objects

---
+++ Megan/wk01/3-wed/scripts.js
@@ -0,0 +1,69 @@
+// Exercise: Objects
+
+// The Recipe Card
+
+// var recipe = {
+// 	title: "Okonomiyaki",
+// 	servings: 2,
+// 	ingredients: ['cabbage', 'ginger', 'egg', 'flour'], 
+// }
+
+// console.log(recipe.title);
+// console.log("Serves: " + recipe.servings);
+// console.log("Ingredients:");
+// console.log(recipe.ingredients[0]);
+// console.log(recipe.ingredients[1]);
+// console.log(recipe.ingredients[2]);
+// console.log(recipe.ingredients[3]);
+
+// The Reading List
+
+// var myBooks = [
+// 	{
+// 		title: "The Goldfinch",
+// 		author: "Donna Tartt",
+// 		alreadyRead: true
+// 	},
+// 	{
+// 		title: "Infinite Jest",
+// 		author: "David Foster Wallace",
+// 		alreadyRead: false
+// 	},
+// 	{
+// 		title: "Slaughterhouse 5",
+// 		author: "Kurt Vonnegut",
+// 		alreadyRead:true
+// 	},
+// ];
+
+//print book and author
+// for (i = 0; i < 3; i++) {
+// 	console.log(myBooks[i].title + " by " + myBooks[i].author);
+// }
+
+
+//print statements based on Boolean
+// for (i = 0; i < 3; i++) {
+// 	if (myBooks[i].alreadyRead === true) {
+// 		console.log("You have read " + myBooks[i].title + " by " + myBooks[i].author);
+// 	} else {
+// 		console.log("You still need to read " + myBooks[i].title + " by " + myBooks[i].author);
+// 	}
+// }
+
+// // The Movie Database
+
+// var myFavouriteFilm = {
+// 	title: "The Shining",
+// 	duration: 146,
+// 	cast: ["Jack Nicholson", " Shelley Duvall", " Scatman Crothers"]
+// };
+
+// function printFilmInfo () {
+// 	console.log(myFavouriteFilm.title + " lasts for " + myFavouriteFilm.duration + " minutes. Stars: " + myFavouriteFilm.cast);
+// }
+
+// printFilmInfo();
+
+
+	
