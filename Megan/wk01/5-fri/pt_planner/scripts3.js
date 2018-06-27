8c1933b8daf2675545226445b3e8d680b7a836b5
Megan McKeough <megan.mckeough@gmail.com>
2018-06-27 19:27:38 +1000

    updated multi line both direction no interchange

--- Megan/wk01/5-fri/pt_planner/scripts3.js
+++ Megan/wk01/5-fri/pt_planner/scripts3.js
@@ -2,31 +2,32 @@
 var origin = "Flinders Street";
 var destination = "Tooronga";
 
-console.log("origin: " + origin);
-console.log("destination: " + destination);
-
-//empty array to push station names for printing
-//DO I STILL NEED THIS???
-// var myJourney = [];
+console.log("Origin: " + origin);
+console.log("Destination: " + destination);
 
 //objects of train lines
 var trainLines = [
 {
 	name: "glenWaverley",
-	stations: ["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"]
+	stations: ["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"],
+	interchange: 3
 },
 {
 	name: "alamein",
-	stations: ["Flinders Street", "Richmond", "Kooyong", "Tooronga"]
+	stations: ["Flinders Street", "Richmond", "Kooyong", "Tooronga"],
+	interchange: 1
 },
 {
 	name: "sandringham",
-	stations: ["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"]
+	stations: ["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"],
+	interchange: 1
 }
 ];
 
+// for comparing if one line or multiple
 var trainLinesJourneyed = [];
 
+//function to find which line travelling on and index of station
 function findLines (stop) {
 	for (i = 0; i < trainLines.length; i++) {
 		if (trainLines[i].stations.includes(stop)) {
@@ -40,6 +41,7 @@ var originIndex = findLines(origin);
 var destinationIndex = findLines(destination);
 // var richmondIndex = 
 
+//printing to console the sliced stations and total stops
 if (originIndex < destinationIndex && trainLinesJourneyed[0] === trainLinesJourneyed[1]) {
 	console.log(trainLines[i].stations.slice(originIndex, destinationIndex + 1));
 	console.log(destinationIndex - originIndex + " stops total");
@@ -49,17 +51,7 @@ if (originIndex < destinationIndex && trainLinesJourneyed[0] === trainLinesJourn
 } 
 			
 
-		// } else if (trainLines[i].stations[i].includes(origin || destination)) {
-		// 	var originIndex = trainLines[i].stations[i].indexOf(origin);
-		// 	var destinationIndex = trainLines[i].stations[i].indexOf(destination);
-		// 	var trainLine = trainLines[i].name;
-		// }
-
 	
-
-//a very convoluted way to get the arrow journey output!
-// 
-
 
 
 
