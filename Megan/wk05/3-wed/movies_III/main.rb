072a6896ef80a425aabf4c46559e86026a86c20e
Megan McKeough <megan.mckeough@gmail.com>
2018-07-19 15:43:34 +1000

    finished movies III - except illegal chars throw error

--- Megan/wk05/3-wed/movies_III/main.rb
+++ Megan/wk05/3-wed/movies_III/main.rb
@@ -31,14 +31,14 @@ end
 get '/:movie' do
 	@movie_input = params[:movie]
 	check_sql = "SELECT * FROM movies WHERE title = '#{ @movie_input }';"
-	result = run_sql(check_sql)
-	if result.count == 0 
+	@result = run_sql(check_sql)
+	if @result.count == 1
+		@movie_result = @result.first 
+	elsif @result.count == 0 
 		@movie_result = HTTParty.get("http://www.omdbapi.com/?t=#{@movie_input}&apikey=2f6435d9")
 		@movie_result = @movie_result.parsed_response
 		add_sql = "INSERT INTO movies (title, director, genre, poster, year, plot) VALUES ('#{ @movie_result['Title'] }', '#{ @movie_result['Director'] }', '#{ @movie_result['Genre'] }', '#{ @movie_result['Poster'] }', '#{ @movie_result['Year'] }', '#{ @movie_result['Plot'] }');"
 		run_sql(add_sql)
-	elsif result.count == 1
-		@movie_result = result.first 
 	end
 	erb :movie
 end
--- Megan/wk05/3-wed/movies_III/movies_III.sql
+++ Megan/wk05/3-wed/movies_III/movies_III.sql
@@ -12,9 +12,9 @@ CREATE TABLE movies (
 
 INSERT INTO movies (title, director, genre, poster, year, plot) VALUES ('Jaws', 'BLah', 'blah', 'blah', 'blah', 'plot');
 
-ALTER TABLE movies RENAME COLUMN "Title" TO "title";
-ALTER TABLE movies RENAME COLUMN "Director" TO "director"
-ALTER TABLE movies RENAME COLUMN "Genre" TO "genre";
-ALTER TABLE movies RENAME COLUMN "Poster" TO "poster";
-ALTER TABLE movies RENAME COLUMN "Year" TO "year";
-ALTER TABLE movies RENAME COLUMN "Plot" TO "plot";
+-- ALTER TABLE movies RENAME COLUMN "Title" TO "title";
+-- ALTER TABLE movies RENAME COLUMN "Director" TO "director"
+-- ALTER TABLE movies RENAME COLUMN "Genre" TO "genre";
+-- ALTER TABLE movies RENAME COLUMN "Poster" TO "poster";
+-- ALTER TABLE movies RENAME COLUMN "Year" TO "year";
+-- ALTER TABLE movies RENAME COLUMN "Plot" TO "plot";
