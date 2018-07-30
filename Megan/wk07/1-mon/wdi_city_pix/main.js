f35d2db495ee63ac80c352c7ef29ec23944b55ea
Megan McKeough <megan.mckeough@gmail.com>
2018-07-31 09:09:25 +1000

    refactored citypix

--- Megan/wk07/1-mon/wdi_city_pix/main.js
+++ Megan/wk07/1-mon/wdi_city_pix/main.js
@@ -1,11 +1,11 @@
 citiesToAdd = ["NYC", "SF", "LA", "SYD", "ATX"]
-classesToAdd = ["nyc", "sf", "la", ".sydney", ".austin"]
+classesToAdd = ["nyc", "sf", "la", "sydney", "austin"]
 
 function addCities () {
 	for (i=0;i<citiesToAdd.length;i++) {
 		var newItem = document.createElement("option");       
 		newItem.textContent = citiesToAdd[i]
-		newItem.classList.add(classesToAdd[i])                                         
+		// newItem.classList.add(classesToAdd[i])                                         
 		document.querySelector('select').appendChild(newItem);
 	}
 }
@@ -16,25 +16,38 @@ var cityDropDown = document.querySelector('#city-type');
 var body = document.querySelector('body')
 
 cityDropDown.addEventListener('change', function(event) {
-	if (event.target.value === "NYC") {
-		body.className = ""
-		body.classList.add('nyc')
-	} else if (event.target.value === "SF") {
-		body.className = ""
-		body.classList.add('sf');
-	} else if (event.target.value === "LA") {
-		body.className = ""
-		body.classList.add('la');
-	} else if (event.target.value === "ATX") {
-		body.className = ""
-		body.classList.add('austin');
-	} else if (event.target.value === "SYD") {
-		body.className = ""
-		body.classList.add('sydney');
-	} else {
-		console.log("whoops")
+	body.className = ""
+	for (i=0;i<classesToAdd.length;i++) {
+		if (event.target.value === citiesToAdd[i]) {
+			body.className = ""
+			body.classList.add(classesToAdd[i])
+		} else {
+			console.log("whoops")
+		}
 	}
 })
+
+
+// cityDropDown.addEventListener('change', function(event) {
+// 	if (event.target.value === "NYC") {
+// 		body.className = ""
+// 		body.classList.add('nyc')
+// 	} else if (event.target.value === "SF") {
+// 		body.className = ""
+// 		body.classList.add('sf');
+// 	} else if (event.target.value === "LA") {
+// 		body.className = ""
+// 		body.classList.add('la');
+// 	} else if (event.target.value === "ATX") {
+// 		body.className = ""
+// 		body.classList.add('austin');
+// 	} else if (event.target.value === "SYD") {
+// 		body.className = ""
+// 		body.classList.add('sydney');
+// 	} else {
+// 		console.log("whoops")
+// 	}
+// })
 
 
 
